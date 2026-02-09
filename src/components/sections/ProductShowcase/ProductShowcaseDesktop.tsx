9ce5889af0a637f879dbec062419434ce9387bf9
Gianni James Favaretto <cpe@justscore.com>
2026-02-09 07:52:12 +0000

    Tablet repsonsive 09/02/26

--- src/components/sections/ProductShowcase/ProductShowcaseDesktop.tsx
+++ src/components/sections/ProductShowcase/ProductShowcaseDesktop.tsx
@@ -23,7 +23,7 @@ export function ProductShowcaseDesktop({
     description?: string;
     className?: string;
   }) => (
-    <FadeReveal className={`flex h-full flex-col p-6 pb-16 md:px-16 md:py-0 xl:px-24 xl:py-0 space-y-4 md:space-y-0 ${className}`}>
+    <FadeReveal className={`flex h-full flex-col p-6 pb-16 lg:px-16 lg:py-0 xl:px-24 xl:py-0 space-y-4 lg:space-y-0 ${className}`}>
       <div className="flex flex-1 items-center">
         <h3 className="heading-base text-primary">{title}</h3>
       </div>
@@ -74,10 +74,10 @@ export function ProductShowcaseDesktop({
   );
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-12 md:gap-32 md:pb-24 bg-background">
-      <div className="md:col-start-1 xl:col-start-3 md:col-span-12 xl:col-span-10">
-        <div className="grid grid-cols-1 md:grid-cols-12">
-          <div className="col-span-1 md:col-span-7">
+    <div className="grid grid-cols-1 lg:grid-cols-12 lg:gap-32 lg:pb-24 bg-background">
+      <div className="lg:col-start-1 xl:col-start-3 lg:col-span-12 xl:col-span-10">
+        <div className="grid grid-cols-1 lg:grid-cols-12">
+          <div className="col-span-6 xl:col-span-7">
             <GalleryImage
               image={project.images[items[0]?.imageIndex ?? 0]}
               index={0}
@@ -85,7 +85,7 @@ export function ProductShowcaseDesktop({
               priority
             />
           </div>
-          <div className="col-span-1 md:col-start-8 md:col-span-5 self-stretch">
+          <div className="col-start-7 xl:col-start-8 col-span-3 xl:col-span-5 self-stretch">
             <GalleryText
               title={items[0]?.title}
               description={items[0]?.description}
@@ -94,16 +94,16 @@ export function ProductShowcaseDesktop({
         </div>
       </div>
 
-      <div className="md:col-start-1 xl:col-start-3 md:col-span-12 xl:col-span-10">
-        <div className="grid grid-cols-1 md:grid-cols-12">
-          <div className="col-span-1 md:col-span-5 self-stretch">
+      <div className="lg:col-start-1 xl:col-start-3 lg:col-span-12 xl:col-span-10">
+        <div className="grid grid-cols-1 lg:grid-cols-12">
+          <div className="col-start-1 col-span-3 xl:col-span-5 self-stretch">
             <GalleryText
               title={items[1]?.title}
               description={items[1]?.description}
-              className="md:text-right md:items-end"
+              className="lg:text-right lg:items-end"
             />
           </div>
-          <div className="col-span-1 md:col-start-6 md:col-span-7">
+          <div className="col-start-4 xl:col-start-5 col-span-6 xl:col-span-7">
             <GalleryImage
               image={project.images[items[1]?.imageIndex ?? 0]}
               index={1}
@@ -113,16 +113,16 @@ export function ProductShowcaseDesktop({
         </div>
       </div>
 
-      <div className="md:col-start-1 xl:col-start-3 md:col-span-12 xl:col-span-10">
-        <div className="grid grid-cols-1 md:grid-cols-12">
-          <div className="col-span-1 md:col-span-7">
+      <div className="lg:col-start-1 xl:col-start-3 lg:col-span-12 xl:col-span-10">
+        <div className="grid grid-cols-1 lg:grid-cols-12">
+          <div className="col-span-6 xl:col-span-7">
             <GalleryImage
               image={project.images[items[2]?.imageIndex ?? 0]}
               index={2}
               total={items.length}
             />
           </div>
-          <div className="col-span-1 md:col-start-8 md:col-span-5 self-stretch">
+          <div className="col-start-7 xl:col-start-8 col-span-3 xl:col-span-5 self-stretch">
             <GalleryText
               title={items[2]?.title}
               description={items[2]?.description}
@@ -131,16 +131,16 @@ export function ProductShowcaseDesktop({
         </div>
       </div>
 
-      <div className="md:col-start-1 xl:col-start-3 md:col-span-12 xl:col-span-10">
-        <div className="grid grid-cols-1 md:grid-cols-12">
-          <div className="col-span-1 md:col-span-5 self-stretch">
+      <div className="lg:col-start-1 xl:col-start-3 lg:col-span-12 xl:col-span-10">
+        <div className="grid grid-cols-1 lg:grid-cols-12">
+          <div className="col-start-1 col-span-3 xl:col-span-5 self-stretch">
             <GalleryText
               title={items[3]?.title}
               description={items[3]?.description}
-              className="md:text-right md:items-end"
+              className="lg:text-right lg:items-end"
             />
           </div>
-          <div className="col-span-1 md:col-start-6 md:col-span-7">
+          <div className="col-start-4 xl:col-start-5 col-span-6 xl:col-span-7">
             <GalleryImage
               image={project.images[items[3]?.imageIndex ?? 0]}
               index={3}
